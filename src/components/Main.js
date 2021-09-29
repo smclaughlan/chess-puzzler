00899cdabeb32940c32a0164ef1e90f8934f18bd
Sean McLaughlan <sean.s.mclaughlan@gmail.com>
2021-09-29 14:11:23 -0700

    Get board from server

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,6 +4,20 @@ import {Button, Container, Paper, Grid, Typography } from '@material-ui/core';
 export default function Main(props) {
   const [board, setBoard] = React.useState();
 
+  React.useEffect(() => {
+    async function getBoardFetch() {
+      console.log('componentDidMount ran');
+      const res = await fetch(`http://localhost:5000/`);
+      if (res.ok) {
+        const bd = await res.json();
+        setBoard(bd);
+      }
+    }
+
+    getBoardFetch();
+  }, []);
+
+
   return (
     <>
       <Container>
