a73bb813052dcce4b620e280e420ba43384ace62
Sean McLaughlan <sean.s.mclaughlan@gmail.com>
2021-09-29 14:54:37 -0700

    Remove unneeded logs and imports

--- src/components/Piece.js
+++ src/components/Piece.js
@@ -1,14 +1,13 @@
 import React from 'react';
-import {Button, Container, Paper, Grid, Typography } from '@material-ui/core';
+import {Typography } from '@material-ui/core';
 
 export default function Piece(props) {
   const [p, setP] = React.useState();
 
   React.useEffect(() => {
-    console.log(props.props);
     setP(props.props);
-    console.log(p);
   }, []);
+
   return (
     <>
       {p && p.pieceType ?
