28f3b3234c92b38de1ff1f8e35af1cfb4531e375
Sean McLaughlan <sean.s.mclaughlan@gmail.com>
2021-09-29 13:20:21 -0700

    Add MaterialUI

---
+++ src/Theme.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { ThemeProvider, createTheme } from '@material-ui/core';
+
+const theme = createTheme({
+  gradientBackground: "linear-gradient(45deg, #8a64c7 30%, #ac9cc7 90%)",
+  fontFamily: "Verdana",
+  primaryColor: '#d33232',
+  secondaryColor: '#f9ac5e',
+  typography: {
+    fontFamily: "Verdana",
+  }
+});
+
+const Theme = props => {
+  return (
+    <ThemeProvider theme={theme}>
+      {props.children}
+    </ThemeProvider>
+  )
+}
+
+export default Theme;
