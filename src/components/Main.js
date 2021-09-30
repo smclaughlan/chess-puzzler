b6aa3f915d5a745edf38c401c30f2fa210a52e31
Sean McLaughlan <sean.s.mclaughlan@gmail.com>
2021-09-30 16:23:03 -0700

    Refactor board rows and locations into separate functions

--- src/components/Main.js
+++ src/components/Main.js
@@ -7,39 +7,42 @@ export default function Main(props) {
 
   React.useEffect(() => {
     async function getBoardFetch() {
-      console.log('componentDidMount ran');
       const res = await fetch(`http://localhost:5000/`);
       if (res.ok) {
         const bd = await res.json();
         setBoard(bd.brd.board);
-        console.log(board);
       }
     }
 
     getBoardFetch();
   }, []);
 
+  function boardRow(rowArr) {
+    return (
+      <Container>
+        {rowArr.map((spaceOrPiece) => {
+          return (boardLocation(spaceOrPiece))
+        })}
+      </Container>
+    )
+  }
+
+  function boardLocation(spaceOrPiece) {
+    return (<Piece props={spaceOrPiece}/>)
+  }
+
 
   return (
     <>
       <Container>
         <Typography variant='h3' align='center'>Chess Puzzler</Typography>
-        <Grid container spacing={2}>
         { board ?
           board.map((rowArr) => {
-            return (
-              rowArr.map((spaceOrPiece) => {
-                console.log('spaceOrPiece: ', spaceOrPiece);
-                return (
-                  <Grid item xs={2}>
-                    <Piece props={spaceOrPiece}/>
-                  </Grid>
-                )
-              }))
-            })
+            return (boardRow(rowArr))
+          })
             :
-            <Typography>No board found. Please reload the page and try again.</Typography> }
-        </Grid>
+            <Typography>No board found. Please reload the page and try again.</Typography>
+        }
       </Container>
     </>
   )
