d89e70ce5aa395bcf127a0c08e9285e048b24b7f
Sean McLaughlan <sean.s.mclaughlan@gmail.com>
2021-10-05 14:35:13 -0700

    Separate pieces from square colors, put pieces on squares

--- src/components/Piece.js
+++ src/components/Piece.js
@@ -2,23 +2,32 @@ import React from 'react';
 
 export default function Piece(props) {
   const [p, setP] = React.useState();
+  const [spaceColor, setSpaceColor] = React.useState();
   const [iconPath, setIconPath] = React.useState();
+  const [squarePath, setSquarePath] = React.useState();
 
   React.useEffect(() => {
-    setP(props.props);
+    setP(props.spaceOrPiece);
+    setSpaceColor(props.spaceColor);
   }, [props]);
 
   React.useEffect(() => {
-    if (p === 'l' || p === 'd') {
-      setIconPath(`./squares/${p}.png`);
-    } else if (p && p.color) {
+    if (p && p.color) {
       setIconPath(`./pieceIcons/${p.color}_${p.pieceType}_.svg`);
     }
+
   }, [p])
 
+  React.useEffect(() => {
+    setSquarePath(`./squares/${props.spaceColor}.png`);
+  }, [props.spaceColor])
+
   return (
-    <>
-      <img alt='chess board' src={iconPath}/>
-    </>
+    <div className="boardParent">
+      {p && p.color ?
+      <img className='boardPiece' alt='chess piece' src={iconPath}/> :
+      <></>}
+      <img className='boardSpace' alt='chess board space' src={squarePath}/>
+    </div>
   )
 }
