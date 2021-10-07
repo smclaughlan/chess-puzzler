3c3fc371196c74c191a026eeb4e0449b9ef0f414
Sean McLaughlan <sean.s.mclaughlan@gmail.com>
2021-10-07 15:01:39 -0700

    StringToBoardPosition complete and working

--- src/utils/validateMove.js
+++ src/utils/validateMove.js
@@ -5,10 +5,34 @@
  * @param {String} str - 'c5', 'a1', etc.
  */
 function stringToBoardPosition(str) {
+  const letters = {
+    'a': 0,
+    'b': 1,
+    'c': 2,
+    'd': 3,
+    'e': 4,
+    'f': 5,
+    'g': 6,
+    'h': 7,
+  }
+  const nums = {
+    '8': 0,
+    '7': 1,
+    '6': 2,
+    '5': 3,
+    '4': 4,
+    '3': 5,
+    '2': 6,
+    '1': 7,
+  }
 
+  const strLetter = str[0];
+  const strNum = str[1];
+
+  return [nums[strNum], letters[strLetter]];
 }
 
-export default function validateMove(moveString, board) {
+function validateMove(moveString, board) {
   moveString.split(' ');
   // If moveString split on space isn't length === 2, return false.
   // If idx[0] of first pair isn't a-h, return false
