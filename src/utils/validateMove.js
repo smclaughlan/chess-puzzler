db76c9c18c8866952bae4644c520b65082653ad4
Sean McLaughlan <sean.s.mclaughlan@gmail.com>
2021-10-07 14:43:26 -0700

    Add function for translating strings to board positions

--- src/utils/validateMove.js
+++ src/utils/validateMove.js
@@ -1,3 +1,19 @@
-export default function validateMove(moveString) {
+/**
+ * Returns the array position of a string.
+ * 'a8' would result in [0, 0].
+ * 'a1' would result in [7, 0].
+ * @param {String} str - 'c5', 'a1', etc.
+ */
+function stringToBoardPosition(str) {
+
+}
+
+export default function validateMove(moveString, board) {
+  moveString.split(' ');
+  // If moveString split on space isn't length === 2, return false.
+  // If idx[0] of first pair isn't a-h, return false
+  // If idx[1] of first pair isn't 1-8, return false
+  // If idx[0] of second pair isn't a-h, return false
+  // If idx[1] of second pair isn't 1-8, return false
 
 }
