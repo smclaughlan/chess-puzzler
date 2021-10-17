2c7385aaefa8c195c8d1702ad7889c90def914b6
Sean McLaughlan <sean.s.mclaughlan@gmail.com>
2021-10-17 15:17:28 -0700

    Update theme

--- src/Theme.js
+++ src/Theme.js
@@ -2,15 +2,26 @@
 import React from 'react';
 import {ThemeProvider, createTheme} from '@material-ui/core';
 
-const theme = createTheme({
-  gradientBackground: 'linear-gradient(45deg, #8a64c7 30%, #ac9cc7 90%)',
+const themeOptions = {
   fontFamily: 'Verdana',
-  primaryColor: '#d33232',
-  secondaryColor: '#f9ac5e',
   typography: {
     fontFamily: 'Verdana',
   },
-});
+  palette: {
+    type: 'light',
+    primary: {
+      main: '#607d8b',
+    },
+    secondary: {
+      main: '#212121',
+    },
+    success: {
+      main: '#00e676',
+    },
+  },
+};
+
+const theme = createTheme(themeOptions);
 
 const Theme = (props) => {
   return (
