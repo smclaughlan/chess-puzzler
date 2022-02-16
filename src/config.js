fdb60ac715c7e2e2bbee258e0b4c8f123f66aaef
Sean McLaughlan <sean.s.mclaughlan@gmail.com>
2022-02-16 14:11:42 -0800

    Update urls

--- src/config.js
+++ src/config.js
@@ -1,9 +1,10 @@
 /* eslint-disable no-undef */
-const production = env.NODE_ENV === 'production';
-const apiBaseUrl = production ? 'https://chesspuzzlerserver.herokuapp.com' : env.SERVER_URL;
-const localBaseUrl = production ? 'https//chesspuzzler.herokuapp.com' : 'http://localhost:3000';
+const production = env.NODE_ENV === "production";
+const apiBaseUrl = production
+  ? "https://chesspuzzlerserver.herokuapp.com/"
+  : env.SERVER_URL;
+const localBaseUrl = production
+  ? "https//chesspuzzler.herokuapp.com/"
+  : "http://localhost:3000/";
 
-export {
-  apiBaseUrl,
-  localBaseUrl,
-};
+export { apiBaseUrl, localBaseUrl };
