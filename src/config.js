df0904395609c55923d2a03f443511e9c57d1e5b
Sean McLaughlan <sean.s.mclaughlan@gmail.com>
2022-02-16 14:49:04 -0800

    Revert urls

--- src/config.js
+++ src/config.js
@@ -1,10 +1,10 @@
 /* eslint-disable no-undef */
 const production = env.NODE_ENV === "production";
 const apiBaseUrl = production
-  ? "https://chesspuzzlerserver.herokuapp.com/"
+  ? "https://chesspuzzlerserver.herokuapp.com"
   : env.SERVER_URL;
 const localBaseUrl = production
-  ? "https//chesspuzzler.herokuapp.com/"
-  : "http://localhost:3000/";
+  ? "https//chesspuzzler.herokuapp.com"
+  : "http://localhost:3000";
 
 export { apiBaseUrl, localBaseUrl };
