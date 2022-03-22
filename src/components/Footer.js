8682b7c6eaa5355bcddcc44a9e544c8b96d8b00c
Sean McLaughlan <sean.s.mclaughlan@gmail.com>
2022-03-21 20:00:11 -0700

    Reformat

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,26 +1,70 @@
 /* eslint-disable require-jsdoc */
-import React from 'react';
-import {AppBar, Container, Grid, Typography} from '@material-ui/core';
+import React from "react";
+import {
+  AppBar,
+  Container,
+  Grid,
+  Typography,
+} from "@material-ui/core";
 
 export default function Footer() {
   return (
-    <AppBar className='footer' position='static' color='primary'>
-      <Container maxWidth='md'>
+    <AppBar
+      className="footer"
+      position="static"
+      color="primary"
+    >
+      <Container maxWidth="md">
         <Grid container spacing={3}>
-          <Grid item xs={4} style={{textAlign: 'center', marginTop: '50px'}}>
-            <Typography color="inherit">© Sean McLaughlan</Typography>
+          <Grid
+            item
+            xs={4}
+            style={{
+              textAlign: "center",
+              marginTop: "50px",
+            }}
+          >
+            <Typography color="inherit">
+              © Sean McLaughlan
+            </Typography>
           </Grid>
-          <Grid item xs={4} style={{textAlign: 'center', marginTop: '50px'}}>
-            <a style={{color: 'white'}} href="https://github.com/smclaughlan/chess-puzzler" target="_blank">
-              <Typography color="inherit">GitHub</Typography>
+          <Grid
+            item
+            xs={4}
+            style={{
+              textAlign: "center",
+              marginTop: "50px",
+            }}
+          >
+            <a
+              style={{ color: "white" }}
+              href="https://github.com/smclaughlan/chess-puzzler"
+              target="_blank"
+            >
+              <Typography color="inherit">
+                GitHub
+              </Typography>
             </a>
           </Grid>
-          <Grid item xs={4} style={{textAlign: 'center', marginTop: '50px'}}>
-            <a style={{color: 'white'}} href="https://smclaughlan.github.io/#four" target="_blank">
-              <Typography color="inherit">Contact</Typography>
+          <Grid
+            item
+            xs={4}
+            style={{
+              textAlign: "center",
+              marginTop: "50px",
+            }}
+          >
+            <a
+              style={{ color: "white" }}
+              href="https://smclaughlan.github.io/#four"
+              target="_blank"
+            >
+              <Typography color="inherit">
+                Contact
+              </Typography>
             </a>
           </Grid>
-        </Grid >
+        </Grid>
       </Container>
     </AppBar>
   );
