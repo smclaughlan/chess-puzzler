4baab691168df57d6896209ded151f289994b2c5
smclaughlan <sean.s.mclaughlan@gmail.com>
2022-10-27 16:23:28 -0700

    Fix urls

--- src/config.js
+++ src/config.js
@@ -1,10 +1,10 @@
 /* eslint-disable no-undef */
 const production = env.NODE_ENV === "production";
 const apiBaseUrl = production
-  ? "https://chess-puzzler-back.onrender.com/"
+  ? "https://chess-puzzler-back.onrender.com"
   : env.SERVER_URL;
 const localBaseUrl = production
-  ? "https://chess-puzzler.vercel.app/"
+  ? "https://chess-puzzler.vercel.app"
   : "http://localhost:3000";
 
 export { apiBaseUrl, localBaseUrl };
