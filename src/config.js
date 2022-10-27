7c5125b6f46640e55e9d4155ed7f6ef7b9b861fd
smclaughlan <sean.s.mclaughlan@gmail.com>
2022-10-27 15:46:40 -0700

    Update backend url for render.com

--- src/config.js
+++ src/config.js
@@ -1,10 +1,10 @@
 /* eslint-disable no-undef */
 const production = env.NODE_ENV === "production";
 const apiBaseUrl = production
-  ? "https://chesspuzzlerserver.herokuapp.com"
+  ? "https://chess-puzzler-back.onrender.com/"
   : env.SERVER_URL;
 const localBaseUrl = production
-  ? "https//chesspuzzler.herokuapp.com"
+  ? "https://chess-puzzler.vercel.app/"
   : "http://localhost:3000";
 
 export { apiBaseUrl, localBaseUrl };
